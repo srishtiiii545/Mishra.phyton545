a4f38ea4797d3b95608404edd32d9098ee792184
srishtiiii545 <155940232+srishtiiii545@users.noreply.github.com>
2024-01-13 12:08:41 +0530

    Created using Colaboratory

---
+++ string.py
@@ -0,0 +1,115 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPlaow7Rz5hDfA+FieiDSEf",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/srishtiiii545/Mishra.phyton545/blob/main/string.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "rs-32XzEKiiP"
+      },
+      "outputs": [],
+      "source": [
+        "a=int(input())\n",
+        "for i in range (1,a):\n",
+        "  def sum(num1, num2)\n",
+        "  return num1+num2\n",
+        "  if i>1:\n",
+        "    for i in range (2,i)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "i=input()\n",
+        "lengthof=len(i)\n",
+        "str=\"\"\n",
+        "\n",
+        "while lengthof>0:\n",
+        "  lengthof-=1\n",
+        "  str+=i[lengthof ]\n",
+        "print(str)\n",
+        "\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "1ozYep7AM4wj",
+        "outputId": "e6cecd9f-6bfc-45df-9690-66682f211fad"
+      },
+      "execution_count": 107,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello\n",
+            "olleh\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "i=input()\n",
+        "x=i.split()\n",
+        "rev=\"\"\n",
+        "\n",
+        "for i in (x):\n",
+        "    lengthof=len(i)\n",
+        "    str=\"\"\n",
+        "\n",
+        "    while lengthof>0:\n",
+        "      lengthof-=1\n",
+        "      str+=i[lengthof ]\n",
+        "    rev+=(str)+\" \"\n",
+        "print(rev)\n",
+        "\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "j0zedMboT33B",
+        "outputId": "530c5315-febc-44bd-97db-a5db677473a0"
+      },
+      "execution_count": 136,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello i am\n",
+            "olleh i ma \n"
+          ]
+        }
+      ]
+    }
+  ]
+}
